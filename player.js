0fe9cf8f8f440b9930126dcb9f131e9550c7b21a
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2021-09-02 00:12:25 -0300

    I have no clue

--- player.js
+++ player.js
@@ -2,6 +2,7 @@ let Player = function(pos = {x:0,y:0}, size = {width:10,height:10}, image, color
   Actor.apply(this,[pos,size,image,color]);
 
   this.speed = 3;
+  this.test = null;
 
   this.update = function(){
     if(game.controller.up){
@@ -24,10 +25,13 @@ let Player = function(pos = {x:0,y:0}, size = {width:10,height:10}, image, color
     for(let i = 0; i < 360; i+=3.6){
       let x = this.pos.x + Math.cos(i*Math.PI/180);
       let y = this.pos.y + Math.sin(i*Math.PI/180);
-      // console.log(x, this.pos.x ,y, this.pos.y);
       let pt = this.ray.cast(game.actors,{x:x,y:y});
+      if(this.test != null && pt != null){
+        game.artist.drawLine(pt.x, pt.y, this.test.x, this.test.y, '#000');
+      }
       if(pt != null){
-        game.artist.drawLine(game.player.pos.x,game.player.pos.y,pt.x,pt.y, '#000');
+        game.artist.drawLine(this.pos.x, this.pos.y, pt.x, pt.y, '#000');
+        this.test = pt;
       }
     }
   }
