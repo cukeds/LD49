a4cccd694ac3e222773f0b1100b19c4803517a3e
Phlip <phlip45@gmail.com>
2021-09-04 13:50:25 -0700

    added particle class

---
+++ particle.js
@@ -0,0 +1,17 @@
+let Particle = function(pos,size,dir,color,updateCallback){
+  this.pos.x = pos.x;
+  this.pos.y = pos.y;
+  this.radius = size;
+  this.dir.x = dir.x;
+  this.dir.y = dir.y;
+  this.color = color;
+  this.life = 5;
+
+  this.update = function(){
+    
+  }
+
+  this.draw = function(){
+
+  }
+}
