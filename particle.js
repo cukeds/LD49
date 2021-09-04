7da3bf4900722e5f6c9d4608fcd554b62c73c746
Phlip <phlip45@gmail.com>
2021-09-04 14:17:37 -0700

    Fixed Particles Class

--- particle.js
+++ particle.js
@@ -1,17 +1,35 @@
-let Particle = function(pos,size,dir,color,updateCallback){
+let Particle = function(pos,size,speed,color,collection,updateCallback){
+  this.id = game.getId();
+  this.pos = {};
+  this.speed = {};
   this.pos.x = pos.x;
   this.pos.y = pos.y;
   this.radius = size;
-  this.dir.x = dir.x;
-  this.dir.y = dir.y;
+  this.speed.x = speed.x;
+  this.speed.y = speed.y;
   this.color = color;
-  this.life = 5;
+  this.life = 160;
+  this.dead = false;
+  this.collection = collection;
 
-  this.update = function(){
-    
+  if(updateCallback != undefined){
+    this.update = updateCallback;
+  }else{
+    this.update = function(delta){
+      if(this.life < 0 && !this.dead){
+        this.dead = true;
+      }
+      this.life -= delta;
+      this.pos.x += this.speed.x * delta;
+      this.pos.y += this.speed.y * delta;
+      this.speed.x /= 1.01;
+      this.speed.y /= 1.01;
+    }
   }
 
   this.draw = function(){
-
+    if(!this.dead){
+      game.artist.drawCircle(this.pos.x,this.pos.y,this.radius,this.color);
+    }
   }
 }
