97581e9f6ffb2915c01ab3ac04f006cabe973fb5
Phlip <phlip45@gmail.com>
2021-10-02 07:40:28 -0700

    My Collision

--- collision.js
+++ collision.js
@@ -10,7 +10,9 @@ let Collision = function(){
 	this.rectangular = function(obj1, obj2){
 		//next to each other;
 		if( this.isRectColliding(obj1,obj2)){
-			if(Math.abs(obj1.pos.x - obj2.pos.x) >= Math.abs(obj1.pos.y - obj2.pos.y)){
+			let diffBetweenX = obj1.pos.x + obj1.width/2 - (obj2.pos.x + obj2.width/2);
+			let diffBetweenY = obj1.pos.y + obj1.height/2 - (obj2.pos.y + obj2.height/2);
+			if(Math.abs(diffBetweenX) <= Math.abs(diffBetweenY)){
 				//left-right collision;
 				return((obj1.pos.x < obj2.pos.x) ? 'left' : 'right');
 			}else{
