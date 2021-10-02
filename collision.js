4838a9ec7e63f5b59200931981de5e566ad6db37
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2021-10-02 04:38:44 -0300

    Collision works now yayyyy

--- collision.js
+++ collision.js
@@ -9,20 +9,51 @@ let Collision = function(){
 	//Detect from Obj 1 moving into Obj2
 	this.rectangular = function(obj1, obj2){
 		//next to each other;
-		if( this.isRectColliding(obj1,obj2)){
-			if(Math.abs(obj1.pos.x - obj2.pos.x) >= Math.abs(obj1.pos.y - obj2.pos.y)){
-				//left-right collision;
-				return((obj1.pos.x < obj2.pos.x) ? 'left' : 'right');
-			}else{
-				//up-down collision
-				return((obj1.pos.y < obj2.pos.y) ? 'top' : 'bottom');
+		if(this.isRectColliding(obj1,obj2)){
+
+			let x1, x2 = null;
+			let y1, y2 = null;
+
+			// Gets the object most to the left in x1, the other in x2
+			if(obj1.pos.x < obj2.pos.x){x1 = obj1; x2 = obj2;}
+			else{x1 = obj2; x2 = obj1;}
+
+			// Gets the object most down in y1, the other in y2
+			if(obj1.pos.y < obj2.pos.y){y1 = obj1; y2 = obj2;}
+			else{y1 = obj2; y2 = obj1;}
+
+			// Width and height of the collision rectangle
+			let width = x1.pos.x + x1.width/2 - (x2.pos.x - x2.width/2);
+			let height = y1.pos.y + y1.height/2 - (y2.pos.y - y2.height/2);
+
+			// // Position of the collision rectangle
+			// let x = x2.pos.x - x2.width * 0.5;
+			// let y = y2.pos.y - y2.height * 0.5;
+
+
+			// Left-right collision
+			if(width < height){
+				if(obj1.pos.x > obj2.pos.x){
+					return 'right';
+				}else{
+					return 'left';
+				}
 			}
+			// top-bottom collision
+			else{
+				if(obj1.pos.y < obj2.pos.y){
+					return 'top';
+				}
+				return 'bottom';
+			}
+
+
 		}else{
 			return false;
 		}
 	}
 
-	this.isRectColliding =function(obj1,obj2){
+	this.isRectColliding = function(obj1,obj2){
 		return obj1.pos.x + obj1.width/2 >= obj2.pos.x - obj2.width/2 &&
 			 obj1.pos.x - obj1.width/2 <= obj2.width/2 + obj2.pos.x &&
 			 obj1.pos.y + obj1.height/2 >= obj2.pos.y - obj2.height/2 &&
