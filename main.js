a32f27d4f0f149b5ba31dd67e2578390baed6c5d
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2021-10-04 13:15:31 -0300

    final boss

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ let game = {
   gridDiv: null, // Division of the grid. Set up on line 30
   seed: "UhOhForgotToSetASeed",
   maxEnemies: 10,
-  sheetsToLoad: ['startButton','testObstacles','emCee','tiles','mats','weapons','businessman', 'player','exit','shooty', 'hitAnims','weaponExplosion','titleScreen'],
+  sheetsToLoad: ['startButton','testObstacles','emCee','tiles','mats','weapons','businessman', 'player','exit','shooty', 'hitAnims','weaponExplosion','titleScreen', 'finalBoss'],
   imagesToLoad: ['inventoryBackdrop','levelBackdrop'],
   collisions: null,
   parser: null,
